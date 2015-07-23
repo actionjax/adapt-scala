2a899b138fd8af5ddbed1bebcf790e7968c2c4a8
antimatter15 <antimatter15@gmail.com>
2015-07-23 19:45:40 -0400

    fixes
    Former-commit-id: e6ac94a8758ef8c8defadcd15489ec3f09157b10

--- src/Main.scala
+++ src/Main.scala
@@ -1,10 +1,14 @@
 
 
-/**
- * @author antimatter15
- */
+import scala.io.Source
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Mean")
+    val src = Source.fromFile("data/cost.csv").getLines
+    val header = src.take(1).next
+    val headers = header.split(",")
+    val closeIndex = headers.toList.indexOf("Close")
+    for(l <- src) {
+      println(l.split(",")(closeIndex))
+    }
   }
 }
